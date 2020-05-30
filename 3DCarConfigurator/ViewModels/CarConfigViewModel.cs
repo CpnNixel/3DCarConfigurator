a18c384a4098ed605758c03a700ac9fd4161059d
Nikita Batarin <batarin.nd@gmail.com>
2020-05-30 20:28:34 +0300

    Add Car Page

--- 3DCarConfigurator/ViewModels/CarConfigViewModel.cs
+++ 3DCarConfigurator/ViewModels/CarConfigViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using _3DCarConfigurator.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace _3DCarConfigurator.Models
 {
@@ -6,5 +9,12 @@ namespace _3DCarConfigurator.Models
     {
         public Car Car { get; set; }
         public IEnumerable<Configuration> Configs { get; set; }
+
+        public ApplicationDbContext context { get; set; }
+        public string configLine { get; set; }
+        public static List<int> elems = new List<int>() { 1, 2 };
+        [BindProperty]
+        public int selected { get; set; }
+        public SelectList listSel = new SelectList(elems);
     }
 }
