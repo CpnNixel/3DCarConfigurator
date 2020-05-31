a7f4273294c824eae3c7e0ca72003e59bd6f7b8f
Nikita Batarin <batarin.nd@gmail.com>
2020-05-31 15:25:34 +0300

    Car Controller

--- 3DCarConfigurator/Controllers/HomeController.cs
+++ 3DCarConfigurator/Controllers/HomeController.cs
@@ -35,41 +35,7 @@ namespace _3DCarConfigurator.Controllers
             return View();
         }
 
-        public IActionResult Cars(int? id)
-        {
-            if (id != null)
-            {
-                return RedirectToAction("Car", new { id = id });
-            }
-            return View(db.Cars.ToList());
-        }
-
-        public IActionResult Car(int id)
-        {
-            Models.Car result = (from kek in db.Cars
-                                 where kek.Id == id
-                                 select kek).FirstOrDefault();
-            db.Users.FirstOrDefault().LikedConfigs = db.Configurations.ToList();
-            db.SaveChanges();
-            CarConfigViewModel cfgViewModel = new CarConfigViewModel { Car = result, Configs = db.Configurations.Where(x => x.CarId == result.Id) };
-
-            foreach (var conf in cfgViewModel.Configs)
-            {
-                string[] arr = conf.DetailsString.Split(",");
-
-                List<Detail> det = new List<Detail>();
-                foreach (var element in arr)
-                {
-                    det.Add(db.Details.Where(x => x.Id == Convert.ToInt32(element)).FirstOrDefault());
-                }
-                conf.Details = det;
-            }
-
-            cfgViewModel.context = db;
-            cfgViewModel.configLine = db.Configurations.Where(x => x.Id == cfgViewModel.Car.CurrentConfigurationId).First().DetailsString;
-
-            return View(cfgViewModel);
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- 3DCarConfigurator/ViewModels/CarConfigViewModel.cs
+++ 3DCarConfigurator/ViewModels/CarConfigViewModel.cs
@@ -10,11 +10,9 @@ namespace _3DCarConfigurator.Models
         public Car Car { get; set; }
         public IEnumerable<Configuration> Configs { get; set; }
 
-        public ApplicationDbContext context { get; set; }
-        public string configLine { get; set; }
-        public static List<int> elems = new List<int>() { 1, 2 };
-        [BindProperty]
-        public int selected { get; set; }
-        public SelectList listSel = new SelectList(elems);
+        public List<Detail> Details { get; set; }
+
+        public List<int> PickedDetails { get; set; }
+       
     }
 }
