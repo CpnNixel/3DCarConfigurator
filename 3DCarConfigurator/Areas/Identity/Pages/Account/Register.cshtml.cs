52d4fe67f49a43bedf91a1d9d69b5409cd22a6fb
Никита Кисель <nkisel549@gmail.com>
2020-06-01 01:40:15 +0300

    Admin Panel

--- 3DCarConfigurator/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ 3DCarConfigurator/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -77,10 +77,11 @@ namespace _3DCarConfigurator.Areas.Identity.Pages.Account
             {
                 var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
+                    await _userManager.AddToRoleAsync(user, "user");
+                    _logger.LogInformation("User created a new account with password.");       
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
--- 3DCarConfigurator/Startup.cs
+++ 3DCarConfigurator/Startup.cs
@@ -63,6 +63,7 @@ namespace _3DCarConfigurator
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
