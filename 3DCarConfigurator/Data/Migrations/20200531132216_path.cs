5c17b978c03e8c2f9159ffbe1edc45d9a500e171
Никита Кисель <nkisel549@gmail.com>
2020-05-31 21:49:24 +0300

    Migrations cleared, Account stuff added

--- 3DCarConfigurator/Data/Migrations/20200531132216_path.cs
+++ 3DCarConfigurator/Data/Migrations/20200531132216_path.cs
@@ -6,26 +6,12 @@ namespace _3DCarConfigurator.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "ImgPath",
-                table: "Configurations",
-                nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "Model3dPath",
-                table: "Configurations",
-                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "ImgPath",
-                table: "Configurations");
-
-            migrationBuilder.DropColumn(
-                name: "Model3dPath",
-                table: "Configurations");
+       
         }
     }
 }
