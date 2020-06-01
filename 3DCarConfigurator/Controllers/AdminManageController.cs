b54299a391dde9588ae207dcc6984b83eea9a7b3
Никита Кисель <nkisel549@gmail.com>
2020-06-01 23:50:54 +0300

    AdminManageFinal

--- 3DCarConfigurator/Controllers/AdminManageController.cs
+++ 3DCarConfigurator/Controllers/AdminManageController.cs
@@ -3,9 +3,7 @@ using _3DCarConfigurator.Models;
 using _3DCarConfigurator.Services;
 using _3DCarConfigurator.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace _3DCarConfigurator.Controllers
@@ -32,27 +30,27 @@ namespace _3DCarConfigurator.Controllers
         public IRepository<Detail> Detail_Repo { get; }
         public IRepository<Configuration> Config_Repo { get; }
 
-        
         [HttpGet]
         public IActionResult Index(ItemsListsViewModel itemsLists)
         {
             ViewBag.CarList = Car_Repo.GetAll().ToList();
             ViewBag.DetailList = Detail_Repo.GetAll().ToList();
             ViewBag.ConfigurationList = Config_Repo.GetAll().ToList();
-            ItemsListsViewModel items = new ItemsListsViewModel() { CarIdSelected = 1, ConfigurationSelected = 1, DetailSelected = 1 };
+            ItemsListsViewModel items = new ItemsListsViewModel() { ItemIdSelected = 1 };
             return View(itemsLists);
         }
+
         [HttpPost]
-        public IActionResult Index(int CarIdSelected)
+        public IActionResult Index(int ItemIdSelected)
         {
-            Car temp = (Car)Car_Repo.GetAll().FirstOrDefault(x => x.Id == CarIdSelected);
+            Car temp = (Car)Car_Repo.GetAll().FirstOrDefault(x => x.Id == ItemIdSelected);
             return EditCar(temp);
         }
 
         [HttpGet]
-        public IActionResult EditCar(int CarIdSelected)
+        public IActionResult EditConfiguration(int ItemIdSelected)
         {
-            Car kek = Car_Repo.GetAll().FirstOrDefault(x => x.Id == CarIdSelected);
+            Configuration kek = Config_Repo.GetAll().FirstOrDefault(x => x.Id == ItemIdSelected);
             if (kek == null)
             {
                 ViewBag.ErrorMesage = $"Car with id {kek.Id} cannot be found";
@@ -62,8 +60,65 @@ namespace _3DCarConfigurator.Controllers
             return View(kek);
         }
 
+        [HttpPost]
+        public IActionResult EditConfiguration(Configuration configuration)
+        {
+            Configuration kek = Config_Repo.GetAll().FirstOrDefault(x => x.Id == configuration.Id);
+            if (kek == null)
+            {
+                ViewBag.ErrorMesage = $"Car with id {configuration.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                Config_Repo.Edit(configuration);
+            }
+            return View("/Views/Home/Index.cshtml");
+        }
 
-     [HttpPost]
+        [HttpGet]
+        public IActionResult EditDetail(int ItemIdSelected)
+        {
+            Detail kek = Detail_Repo.GetAll().FirstOrDefault(x => x.Id == ItemIdSelected);
+            if (kek == null)
+            {
+                ViewBag.ErrorMesage = $"Car with id {kek.Id} cannot be found";
+                return View("NotFound");
+            }
+
+            return View(kek);
+        }
+
+        [HttpPost]
+        public IActionResult EditDetail(Detail detail)
+        {
+            Detail kek = Detail_Repo.GetAll().FirstOrDefault(x => x.Id == detail.Id);
+            if (kek == null)
+            {
+                ViewBag.ErrorMesage = $"Car with id {kek.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                Detail_Repo.Edit(kek);
+            }
+            return View("/Views/Home/Index.cshtml");
+        }
+
+        [HttpGet]
+        public IActionResult EditCar(int ItemIdSelected)
+        {
+            Car kek = Car_Repo.GetAll().FirstOrDefault(x => x.Id == ItemIdSelected);
+            if (kek == null)
+            {
+                ViewBag.ErrorMesage = $"Car with id {kek.Id} cannot be found";
+                return View("NotFound");
+            }
+
+            return View(kek);
+        }
+
+        [HttpPost]
         public IActionResult EditCar(Car car)
         {
             Car kek = Car_Repo.GetAll().FirstOrDefault(x => x.Id == car.Id);
